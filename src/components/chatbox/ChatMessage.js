069147c3e2a51319ddb5bbc61b4300ecae58c8b6
Adrien Many <adrien.many@utip.io>
2021-02-09 16:59:13 +0100

    chatmessage style

--- src/components/chatbox/ChatMessage.js
+++ src/components/chatbox/ChatMessage.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ChatMessage(props) {
     return (
-      <div>
-          <p>{props.user}</p>
-          <p>{props.message}</p>
+      <div className="chatmessage-container">
+          <span className="chatmessage-user">{props.user}: </span>
+          <span className="chatMessage-message">{props.message}</span>
       </div>
   )
 }
