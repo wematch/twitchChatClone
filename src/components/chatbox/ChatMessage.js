c7300a5ebe195758009702eb2aa1535e4b1ce9ab
Adrien Many <adrien.many@utip.io>
2021-02-10 09:44:10 +0100

    fix timestamp

--- src/components/chatbox/ChatMessage.js
+++ src/components/chatbox/ChatMessage.js
@@ -2,8 +2,12 @@ import React from "react";
 import moment from "moment";
 
 export default function ChatMessage(props) {
+  let relativeDate = "";
+  if (props.createdAt !== null) {
+    relativeDate = moment(new Date(props.createdAt.seconds * 1000)).fromNow();
+  }
   return (
-    <div className="chatmessage-container">
+    <div className="chatmessage-container" title={relativeDate}>
       <span className="chatmessage-user" style={{ color: props.userColor }}>
         {props.user}:{" "}
       </span>
